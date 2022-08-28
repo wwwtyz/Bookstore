0ee96a4001098dbc8606f05ad059f640877b5145
wwwtyz <wwwtyz@tut.by>
2022-08-28 15:14:10 +0300

    feat add input component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
-
+import React from "react";
+import { Input } from "./components/Input/Input";
 
 function App() {
   return (
-    <div>Works</div>
+    <div>
+      <Input id={"id"} label={"Hello"} placeholder={"Works"}></Input>
+    </div>
   );
 }
 
