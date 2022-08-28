dea3a08f8c5ef47112d365f8bf34b032d676f61c
wwwtyz <wwwtyz@tut.by>
2022-08-28 21:04:47 +0300

    add footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { FooterContainer, FooterSection } from "./footer.styled";
+
+export function Footer() {
+  return (
+    <FooterContainer>
+      <FooterSection>©2022 Bookstore</FooterSection>
+      <FooterSection>All rights reserved</FooterSection>
+    </FooterContainer>
+  );
+}
---
+++ src/components/Footer/footer.styled.ts
@@ -0,0 +1,15 @@
+import styled from "@emotion/styled";
+
+export const FooterContainer = styled.footer`
+  border-top: 1px solid var(--bs-color-border);
+  display: flex;
+  justify-content: space-between;
+  padding: var(--bs-spacing-4);
+  max-width: 1120px;
+  margin: auto;
+  width: 100%;
+`;
+
+export const FooterSection = styled.div`
+  color: var(--bs-color-text-disabled);
+`;
